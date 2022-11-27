f5611231c9fd360fbfb41542ab99432ff16110aa
sharif291 <talha.halcyon@gmail.com>
2022-11-27 14:51:15 +0600

    product

--- controller/products.js
+++ controller/products.js
@@ -1,126 +1,5 @@
 const Product = require('../models/productModel');
 
-const createProduct = async (req, res) => {
-  const {
-    title,
-    code,
-    description,
-    regularPrice,
-    weight,
-    discountPrice,
-    categories,
-    size,
-    stockQuantity,
-    shippingRateDhaka,
-    shippingRateOut,
-    images,
-    features,
-    featuresDetails,
-    thumbnailImage,
-  } = req.body;
-
-  if (
-    (!title || !code || !description || !regularPrice || !categories,
-    !weight ||
-      !discountPrice ||
-      !size ||
-      !stockQuantity ||
-      !shippingRateDhaka ||
-      !shippingRateOut ||
-      !images ||
-      !features ||
-      !thumbnailImage)
-  ) {
-    return res.status(400).json({ message: 'Invalid Data!' });
-  }
-  try {
-    const newProduct = new Product({
-      title,
-      code,
-      description,
-      weight,
-      regularPrice,
-      discountPrice,
-      price: Number(regularPrice) - Number(discountPrice),
-      categories,
-      size,
-      thumbnailImage,
-      images,
-      features,
-      featuresDetails,
-      stockQuantity,
-      shippingRateDhaka,
-      shippingRateOut,
-      shippingInDhaka: Number(shippingRateDhaka) * Number(weight),
-      shippingOutDhaka: Number(shippingRateOut) * Number(weight),
-    });
-
-    await newProduct.save();
-
-    return res.status(201).json({
-      message: 'Product Created Successfully',
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const updateProduct = async (req, res) => {
-  const { productId } = req.params;
-
-  const {
-    title,
-    code,
-    description,
-    regularPrice,
-    weight,
-    discountPrice,
-    categories,
-    size,
-    stockQuantity,
-    shippingRateDhaka,
-    shippingRateOut,
-    images,
-    features,
-    featuresDetails,
-    thumbnailImage,
-  } = req.body;
-
-  const updatedData = {
-    title,
-    code,
-    description,
-    weight,
-    regularPrice,
-    discountPrice,
-    price: Number(regularPrice) - Number(discountPrice),
-    categories,
-    size,
-    thumbnailImage,
-    images,
-    features,
-    featuresDetails,
-    stockQuantity,
-    shippingRateDhaka,
-    shippingRateOut,
-    shippingInDhaka: Number(shippingRateDhaka) * Number(weight),
-    shippingOutDhaka: Number(shippingRateOut) * Number(weight),
-  };
-
-  try {
-    await Product.updateOne(
-      { _id: productId },
-      {
-        $set: updatedData,
-      }
-    );
-
-    res.status(200).json({ message: 'Product Updated Successfully!' });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 const getProducts = async (req, res) => {
   const query = req.query;
 
@@ -158,22 +37,8 @@ const productById = async (req, res) => {
   }
 };
 
-const deleteProduct = async (req, res) => {
-  const { productId } = req.params;
-  try {
-    await Product.deleteOne({ _id: productId });
-
-    res.status(203).json({ message: 'Product  delete successful.' });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 module.exports = {
   getProducts,
   productPagination,
-  createProduct,
-  updateProduct,
-  deleteProduct,
   productById,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,8 @@
 const {
   getProducts,
-  createProduct,
-  updateProduct,
-  deleteProduct,
   productById,
   productPagination,
 } = require('../controller/products');
-const { protect } = require('../middleware/authenticate');
-const { uploadImage, multiUploadImage } = require('../middleware/fileUpload');
-
 const router = require('express').Router();
 
 // @desc    Find Product
@@ -25,20 +19,5 @@ router.get('/pagination', productPagination);
 // @route   GET /api/v1/products/:key
 // @access  Public
 router.get('/:key', productById);
-
-// @desc    Create Products
-// @route   POST /api/v1/products
-// @access  Private
-router.post('/', protect, createProduct);
-
-// @desc    Update Product
-// @route   PUT /api/v1/products/:productId
-// @access  Private
-router.put('/:productId', protect, updateProduct);
-
-// @desc    Delete Product
-// @route   PUT /api/v1/products/:productId
-// @access  Private
-router.delete('/:productId', protect, deleteProduct);
 
 module.exports = router;
